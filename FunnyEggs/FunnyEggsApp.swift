02353f696a2b31fd54bf3463dc98e069fb7675de
Uliana Gritchina <ugritchina@icloud.com>
2022-05-28 15:40:17 +0300

    Initial Commit

---
+++ FunnyEggs/FunnyEggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyEggsApp.swift
+//  FunnyEggs
+//
+//  Created by Ульяна Гритчина on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FunnyEggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
